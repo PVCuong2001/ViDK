94e4169a9df3a3380bc6bef0729f6cfe5120aad5
PVCuong <cuongphanit2001@gmail.com>
2022-04-28 18:57:00 +0700

    Tam on dung 2 con esp8266 moi duoc

--- Demo_ESP8266/src/main.cpp
+++ Demo_ESP8266/src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 #include "DHT.h"
 #include <Adafruit_Sensor.h>
@@ -16,9 +17,9 @@ float total_hum = 0;
 float mycount = 0;
 
 WebSocketsClient webSocket;
-// const char *ssid = "Wifi";              //Đổi thành wifi của bạn
-// const char *password = "PVCuong016867"; //Đổi pass luôn
-// const char *ip_host = "192.168.1.19";   //Đổi luôn IP host của PC nha
+const char *ssid = "Wifi";              //Đổi thành wifi của bạn
+const char *password = "PVCuong016867"; //Đổi pass luôn
+const char *ip_host = "192.168.1.19";   //Đổi luôn IP host của PC nha
 // const char *ssid = "pass=MD5('12341234')"; //Đổi thành wifi của bạn
 // const char *password = "loi!1234";         //Đổi pass luôn
 // const char *ip_host = "192.168.43.218";    //Đổi luôn IP host của PC nha
@@ -26,10 +27,10 @@ WebSocketsClient webSocket;
 // const char *password = "2001Cuong"; //Đổi thành wifi của bạn        //Đổi pass luôn
 // const char *ip_host = "192.168.45.218";
 
-const char *ssid = "???";               //Đổi thành wifi của bạn
-const char *password = "06202001";      //Đổi pass luôn
-const char *ip_host = "192.168.43.131"; //Đổi luôn IP host của PC nha
-const uint16_t port = 3000;             // Port thích đổi thì phải đổi ở server nữa
+// const char *ssid = "???";               //Đổi thành wifi của bạn
+// const char *password = "06202001";      //Đổi pass luôn
+// const char *ip_host = "192.168.43.131"; //Đổi luôn IP host của PC nha
+const uint16_t port = 3000; // Port thích đổi thì phải đổi ở server nữa
 bool is_auto = false;
 float T1 = 25;
 float T2 = 28;
@@ -68,116 +69,104 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
     string label = "";
     string data = "";
     string num_str1 = "";
-    switch (type)
+    try
     {
-    case WStype_DISCONNECTED:
-        Serial.printf("[WSc] Disconnected!\n");
-        break;
-    case WStype_CONNECTED:
+        switch (type)
+        {
+        case WStype_DISCONNECTED:
+            Serial.printf("[WSc] Disconnected!\n");
+            break;
+        case WStype_CONNECTED:
+        {
+            Serial.printf("[WSc] Connected to url: %s\n", payload);
+            break;
+        }
+
+        case WStype_TEXT:
+            if (payload)
+            {
+
+                if (std::string((char *)payload).find("SetT1") != string::npos)
+                {
+                    Serial.println("SetT1");
+                    string s = string((char *)payload);
+                    T1 = stoi(string((char *)payload).substr(6, s.size()));
+                    label = "T1:";
+                    data = std::to_string(T1);
+                }
+                else if (std::string((char *)payload).find("SetT2") != string::npos)
+                {
+                    Serial.println("SetT2");
+                    string s = string((char *)payload);
+                    T2 = stoi(string((char *)payload).substr(6, s.size()));
+                    label = "T2:";
+                    data = std::to_string(T2);
+                }
+                else if (std::string((char *)payload).find("SetT3") != string::npos)
+                {
+                    Serial.println("SetT3");
+                    string s = string((char *)payload);
+                    T3 = stoi(string((char *)payload).substr(6, s.size()));
+                    label = "T3:";
+                    data = std::to_string(T3);
+                }
+                else if (strcmp((char *)payload, "AutoOff") == 0)
+                {
+                    Serial.println("AutoOFf");
+                    is_auto = false;
+                    label = "auto:";
+                    data = "off";
+                }
+                else if (strcmp((char *)payload, "AutoOn") == 0)
+                {
+                    Serial.println("AutoOn");
+                    is_auto = true;
+                    label = "auto:";
+                    data = "on";
+                }
+                else if (strcmp((char *)payload, "Speed0") == 0)
+                {
+                    Serial.printf("Speed0");
+                    SpeedState(0);
+                    label = "Speed0:";
+                    data = "none";
+                }
+                else if (strcmp((char *)payload, "Speed1") == 0)
+                {
+                    Serial.printf("Speed1");
+                    SpeedState(1);
+                    label = "Speed1:";
+                    data = "none";
+                }
+                else if (strcmp((char *)payload, "Speed2") == 0)
+                {
+                    Serial.printf("Text off");
+                    SpeedState(2);
+                    label = "Speed2:";
+                    data = "none";
+                }
+                else if (strcmp((char *)payload, "Speed3") == 0)
+                {
+                    Serial.printf("Speed3");
+                    SpeedState(3);
+                    label = "Speed3:";
+                    data = "none";
+                }
+                else if (strcmp((char *)payload, "Init") == 0)
+                {
+                    label = "init:";
+                    data = std::to_string(is_auto) + " " + std::to_string(T1) + " " + std::to_string(T2) + " " + std::to_string(T3);
+                }
+                num_str1 = label + data;
+                const char *convertido = num_str1.c_str();
+                webSocket.sendTXT(convertido);
+            }
+            break;
+        }
+    }
+    catch (...)
     {
-        Serial.printf("[WSc] Connected to url: %s\n", payload);
     }
-    break;
-    case WStype_TEXT:
-        Serial.printf("[WSc] get text: %s\n", payload);
-
-        if (std::string((char *)payload).find("SetT1") != string::npos)
-        {
-            Serial.println("SetT1");
-            string s = string((char *)payload);
-            T1 = stoi(string((char *)payload).substr(6, s.size()));
-            label = "T1:";
-            data = std::to_string(T1);
-        }
-        else if (std::string((char *)payload).find("SetT2") != string::npos)
-        {
-            Serial.println("SetT2");
-            string s = string((char *)payload);
-            T2 = stoi(string((char *)payload).substr(6, s.size()));
-            label = "T2:";
-            data = std::to_string(T2);
-        }
-        else if (std::string((char *)payload).find("SetT3") != string::npos)
-        {
-            Serial.println("SetT3");
-            string s = string((char *)payload);
-            T3 = stoi(string((char *)payload).substr(6, s.size()));
-            label = "T3:";
-            data = std::to_string(T3);
-        }
-        else if (strcmp((char *)payload, "AutoOff") == 0)
-        {
-            Serial.println("AutoOFf");
-            is_auto = false;
-            label = "auto:";
-            data = "off";
-        }
-        else if (strcmp((char *)payload, "AutoOn") == 0)
-        {
-            Serial.println("AutoOn");
-            is_auto = true;
-            label = "auto:";
-            data = "on";
-        }
-        else if (strcmp((char *)payload, "Speed0") == 0)
-        {
-            Serial.printf("Speed0");
-            SpeedState(0);
-            label = "Speed0:";
-            data = "none";
-        }
-        else if (strcmp((char *)payload, "Speed1") == 0)
-        {
-            Serial.printf("Speed1");
-            SpeedState(1);
-            label = "Speed1:";
-            data = "none";
-        }
-        else if (strcmp((char *)payload, "Speed2") == 0)
-        {
-            Serial.printf("Text off");
-            SpeedState(2);
-            label = "Speed2:";
-            data = "none";
-        }
-        else if (strcmp((char *)payload, "Speed3") == 0)
-        {
-            Serial.printf("Speed3");
-            SpeedState(3);
-            label = "Speed3:";
-            data = "none";
-        }
-        else if (strcmp((char *)payload, "Init") == 0)
-        {
-            label = "init:";
-            data = std::to_string(is_auto) + " " + std::to_string(T1) + " " + std::to_string(T2) + " " + std::to_string(T3);
-        }
-        else if (strcmp((char *)payload, "DoorOn") == 0)
-        {
-            servo.write(180);
-            checkDoor = true;
-            timer = millis();
-            label = "door:";
-            data = "on";
-        }
-        else if (strcmp((char *)payload, "DoorOff") == 0)
-        {
-            servo.write(0);
-            checkDoor = false;
-            label = "door:";
-            data = "off";
-        }
-
-        break;
-    case WStype_BIN:
-        Serial.printf("[WSc] get binary length: %u\n", length);
-        Serial.printf("[WSc] get text: %s\n", payload);
-
-        break;
-    }
-    num_str1 = label + data;
-    const char *convertido = num_str1.c_str();
-    webSocket.sendTXT(convertido);
 }
 void setup()
 {
@@ -199,16 +188,11 @@ void setup()
     }
     webSocket.begin(ip_host, port);
     webSocket.onEvent(webSocketEvent);
+    webSocket.setAuthorization(WiFi.macAddress().c_str());
     dht.begin();
 }
 void loop()
 {
-    if (checkDoor == true && millis() - timer > 5000)
-    {
-        servo.write(0);
-        checkDoor = false;
-        webSocket.sendTXT("door:off");
-    }
 
     webSocket.loop();
     float h = dht.readHumidity();
--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,15 +1,126 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <string>
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include <WebSocketsClient.h>
+#include <Servo.h>
+
+WebSocketsClient webSocket;
+const char *ssid = "Wifi";              //Đổi thành wifi của bạn
+const char *password = "PVCuong016867"; //Đổi pass luôn
+const char *ip_host = "192.168.1.19";
+const uint16_t port = 3000;
+
+const uint8_t servoPin = D2;
+Servo servo;
+
+//----DHT11----//
+#define DHTTYPE DHT11
+#define DHTPIN 13
+DHT dht(DHTPIN, DHTTYPE);
+
+bool is_auto = false;
+float T1 = 25;
+float T2 = 28;
+float T3 = 30;
+
+float total_temp = 0;
+float total_hum = 0;
+float mycount = 0;
+unsigned long mytime = 0;
+//----DHT 11----//
+
 #define SS_PIN D8
 #define RST_PIN D0
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key;
+
+bool checkDoor = false;
+
 // Init array that will store new NUID
 byte nuidPICC[4];
-#define LED 3
-int check = 0;
-int isturnoff = 0;
+#define LED D3
+int checkRFID = 0;
+int isRfidTurnOff = 0;
 unsigned long timer, timer2 = 0;
+
+void SpeedState(int status)
+{
+  // switch (status)
+  // {
+  // case 0:
+  //   digitalWrite(D5, 0); // Khi client phát sự kiện "LED_ON" thì server sẽ bật LED
+  //   digitalWrite(D6, 0);
+  //   digitalWrite(D7, 0);
+  //   break;
+  // case 1:
+  //   digitalWrite(D5, 1); // Khi client phát sự kiện "LED_ON" thì server sẽ bật LED
+  //   digitalWrite(D6, 0);
+  //   digitalWrite(D7, 0);
+  //   break;
+  // case 2:
+  //   digitalWrite(D5, 1); // Khi client phát sự kiện "LED_ON" thì server sẽ bật LED
+  //   digitalWrite(D6, 1);
+  //   digitalWrite(D7, 0);
+  //   break;
+  // case 3:
+  //   digitalWrite(D5, 1); // Khi client phát sự kiện "LED_ON" thì server sẽ bật LED
+  //   digitalWrite(D6, 1);
+  //   digitalWrite(D7, 1);
+  //   break;
+  // }
+}
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+  std::string label = "";
+  std::string data = "";
+  std::string num_str1 = "";
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[WSc] Disconnected!\n");
+    break;
+  case WStype_CONNECTED:
+  {
+    Serial.printf("[WSc] Connected to url: %s\n", payload);
+  }
+  break;
+  case WStype_TEXT:
+
+    Serial.printf("[WSc] get text: %s\n", payload);
+    if (strcmp((char *)payload, "DoorOn") == 0)
+    {
+      servo.write(180);
+      checkDoor = true;
+      digitalWrite(LED, HIGH);
+      timer = millis();
+      label = "door:";
+      data = "on";
+    }
+    else if (strcmp((char *)payload, "DoorOff") == 0)
+    {
+      servo.write(0);
+      checkDoor = false;
+      digitalWrite(LED, LOW);
+      label = "door:";
+      data = "off";
+    }
+
+    break;
+  case WStype_BIN:
+    Serial.printf("[WSc] get binary length: %u\n", length);
+    Serial.printf("[WSc] get text: %s\n", payload);
+
+    break;
+  }
+  num_str1 = label + data;
+  const char *convertido = num_str1.c_str();
+  webSocket.sendTXT(convertido);
+}
+
 void setup()
 {
   Serial.begin(9600); // Initiate a serial communication
@@ -18,25 +129,38 @@ void setup()
   Serial.println("Approximate your card to the reader...");
   Serial.println();
   pinMode(LED, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  // WiFi.begin(ssid);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  webSocket.begin(ip_host, port);
+  webSocket.onEvent(webSocketEvent);
+
+  servo.attach(servoPin);
+  // dht.begin();
   // digitalWrite(LED, LOW);
 }
 void loop()
 {
-
-  if (isturnoff == 1)
+  webSocket.loop();
+  if (isRfidTurnOff == 1)
   {
     digitalWrite(LED, LOW);
     delay(2000);
-    isturnoff = 0;
-    check = 0;
+    isRfidTurnOff = 0;
+    checkRFID = 0;
   }
-  else if (check == 1)
+  else if (checkRFID == 1)
   {
     Serial.println("High");
     digitalWrite(LED, HIGH);
     delay(300);
   }
-  else if (check == 2)
+  else if (checkRFID == 2)
   {
     Serial.println("Low-low");
     digitalWrite(LED, LOW);
@@ -45,51 +169,49 @@ void loop()
     digitalWrite(LED, HIGH);
     delay(300);
   }
-  // Look for new cards
-  if (!rfid.PICC_IsNewCardPresent())
-  {
 
-    return;
-  }
-  // Select one of the cards
-  if (!rfid.PICC_ReadCardSerial())
+  if (rfid.PICC_IsNewCardPresent())
   {
-    return;
-  }
-  // Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content = "";
-  byte letter;
-  for (byte i = 0; i < rfid.uid.size; i++)
-  {
-    Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(rfid.uid.uidByte[i], HEX);
-    content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(rfid.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == "E1 71 88 20") // change here the UID of the card/cards that you want to give access
-  {
-    Serial.println("Authorized access");
-    if (check == 1)
+    if (rfid.PICC_ReadCardSerial())
     {
-      if (millis() - timer < 1000)
+      Serial.print("UID tag :");
+      String content = "";
+
+      for (byte i = 0; i < rfid.uid.size; i++)
       {
-        isturnoff = 1;
+        content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+        content.concat(String(rfid.uid.uidByte[i], HEX));
+      }
+      content.toUpperCase();
+      if (content.substring(1) == "E1 71 88 20") // change here the UID of the card/cards that you want to give access
+      {
+        Serial.println("Authorized access");
+        if (checkRFID == 1)
+        {
+          if (millis() - timer2 < 1000)
+          {
+            isRfidTurnOff = 1;
+          }
+        }
+        timer2 = millis();
+        checkRFID = 1;
+        // isturnoff +=1;
+        Serial.println(isRfidTurnOff);
+      }
+
+      else
+      {
+        Serial.println("Authorized denied");
+        checkRFID = 2;
+        isRfidTurnOff = 0;
       }
     }
-    timer = millis();
-    check = 1;
-    // isturnoff +=1;
-    Serial.println(isturnoff);
   }
 
-  else
+  if (checkDoor == true && millis() - timer > 5000)
   {
-    Serial.println("Authorized denied");
-    check = 2;
-    isturnoff = 0;
+    servo.write(0);
+    checkDoor = false;
+    webSocket.sendTXT("door:off");
   }
 }
